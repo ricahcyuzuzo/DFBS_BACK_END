bb4ba3d7d12d0676ad627ab905d7d28d35b1ded4
Fabrice Niyongabo <ninga.fab@gmail.com>
2022-09-06 18:51:09 +0200

    fix

--- model/transactions.js
+++ model/transactions.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   amountPaid: { type: String },
   userId: { type: String, required: true },
   amountToGet: { type: String, required: true },
+  phoneOrAccount: { type: String, required: true },
   status: { type: String, required: true },
   received: { type: Boolean, required: true, default: false },
   createdAt: { type: String, default: new Date() },
--- routes/transactions.js
+++ routes/transactions.js
@@ -13,4 +13,29 @@ router.get("/all/", auth, async (req, res) => {
   }
 });
 
+router.post("/save/", auth, async (req, res) => {
+  const {
+    fromCurrency,
+    phoneOrAccount,
+    toCurrency,
+    amountPaid,
+    amountToGet,
+    status,
+  } = req.body;
+  try {
+    const transactions = await Transactions.create({
+      fromCurrency,
+      phoneOrAccount,
+      toCurrency,
+      amountPaid,
+      amountToGet,
+      status,
+      userId: req.user.user_id,
+    });
+    return res.status(200).send({ msg: "transaction saved!" });
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
+  }
+});
+
 module.exports = router;
